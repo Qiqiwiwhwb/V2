d153b6a4664c7634f1dd4558f2e3b60f96cf8fcd
Qiqiwiwhwb <94961224+Qiqiwiwhwb@users.noreply.github.com>
2021-12-14 03:02:05 +0000

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "cPkHQT5sscVPyd+KPyzzyw==",
+	"serverToken": "1@KYKvLSLUwv67s6fCeCUoeXQT6glYMP/5SOIpdlC4lnAUxFQQhCmgt2fuec51wwg1yldJLgDI90LkIA==",
+	"clientToken": "lLus/IKwqCYFkKqV6PxHedRe74BoklgUdjf5ZVPTV2E=",
+	"encKey": "65JzJuR7RCRD0rQhnxbc1f/LFoMwewpaViiFqWl8o50=",
+	"macKey": "0yac8JSVuOgHyVyxgWnY2Mk3fbkq2r7SmTLhWuY9mEA="
+}
